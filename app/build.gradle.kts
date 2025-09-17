cebc944781735978d4ec7d8d8f3ff9c8de0487a9
Tony F. <a20223209@pucp.edu.pe>
2025-09-17 09:23:47 -0500

    Se agregan las dependencias del proyecto y las librerias a los archivos Gradle. Asimismo, se actualiza el .gitignore y el archivo de configuraciones de ejecucion compartidas
    Added new dependencies for RecyclerView, Lifecycle, Retrofit, and Gson in build.gradle.kts and libs.versions.toml. Enabled Jetifier in gradle.properties. Updated Android Gradle Plugin and library versions. Added .idea/.gitignore and runConfigurations.xml for IDE configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,30 +1,24 @@
-plugins {
-    alias(libs.plugins.android.application)
-}
+plugins { alias(libs.plugins.android.application) }
 
 android {
     namespace = "com.example.connectifyproject"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.example.connectifyproject"
         minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables { useSupportLibrary = true }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    buildFeatures { viewBinding = true; buildConfig = true }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,11 +26,21 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.core)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.recyclerview)
     implementation(libs.constraintlayout)
+
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.runtime)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
